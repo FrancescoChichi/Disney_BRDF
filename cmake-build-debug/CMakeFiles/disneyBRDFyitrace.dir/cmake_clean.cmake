7297d8c9da671b0a8f5144d5651c74d52075c30a
francesco <francesco.chichi@gmail.com>
2018-01-03 22:35:34 +0100

    o

---
+++ cmake-build-debug/CMakeFiles/disneyBRDFyitrace.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/disneyBRDFyitrace.dir/apps/disneyBRDFyitrace.cpp.o"
+  "../bin/disneyBRDFyitrace.pdb"
+  "../bin/disneyBRDFyitrace"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/disneyBRDFyitrace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
