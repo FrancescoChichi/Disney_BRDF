980cf15a08fc23f88d7555f1319aa1126443f803
Francesco Chichi <francesco.chichi@gmail.com>
2018-07-02 18:45:28 +0200

    n

---
+++ gcc5.sh
@@ -0,0 +1,6 @@
+ sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60
+ sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 40
+ sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 60
+ sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 40
+
+
---
+++ gcc7.sh
@@ -0,0 +1,6 @@
+ sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 40
+ sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 60
+ sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 40
+ sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 60
+
+
